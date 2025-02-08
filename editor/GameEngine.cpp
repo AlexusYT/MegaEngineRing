3bdc6f9fee05028c5244c5f7c1ac9e7dc997d275
AlexusYT <93870232+AlexusYT@users.noreply.github.com>
2025-02-08 22:54:04 +0500

    MER-52: Add ImGui

--- editor/GameEngine.cpp
+++ editor/GameEngine.cpp
@@ -29,9 +29,14 @@
 #if defined _WIN32
 	#include <windows.h>
 #endif
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <thread>
 
 #include "Globals.h"
 #include "cmdOptions/EngineOptionGroup.h"
+#include "examples/libs/glfw/include/GLFW/glfw3.h"
 #include "mvp/ApplicationController.h"
 #include "mvp/IApplicationController.h"
 #include "mvp/contexts/ApplicationContext.h"
@@ -39,12 +44,115 @@
 
 namespace mer::editor::ui {
 
+static void glfw_error_callback(int error, const char* description) {
+	fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
 class GameEngineImpl {
 	static inline std::shared_ptr<mvp::IApplicationController> appController;
 	static inline std::unordered_set<std::shared_ptr<mvp::IPresenter>> windows;
 
 
 public:
+	static void runImgui() {
+		glfwSetErrorCallback(glfw_error_callback);
+		if (!glfwInit()) return;
+
+
+		// GL 3.0 + GLSL 130
+		const char* glsl_version = "#version 130";
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+		//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+		//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+
+		// Create window with graphics context
+		GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
+		if (window == nullptr) return;
+		glfwHideWindow(window);
+		glfwMakeContextCurrent(window);
+		glfwSwapInterval(1); // Enable vsync
+
+		// Setup Dear ImGui context
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGuiIO &io = ImGui::GetIO();
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
+		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+		// Setup Dear ImGui style
+		ImGui::StyleColorsDark();
+		//ImGui::StyleColorsLight();
+
+		// Setup Platform/Renderer backends
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init(glsl_version);
+
+		// Our state
+
+		// Main loop
+		while (!glfwWindowShouldClose(window)) {
+			// Poll and handle events (inputs, window resize, etc.)
+			// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+			// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+			// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+			// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+			glfwPollEvents();
+			if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0) {
+				ImGui_ImplGlfw_Sleep(10);
+				continue;
+			}
+
+			// Start the Dear ImGui frame
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+
+			//if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
+
+			// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+			{
+				static int counter = 0;
+				ImGui::DockSpaceOverViewport(0, ImGui::GetMainViewport());
+				ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+				ImGui::Text("This is some useful text.");
+
+
+				if (ImGui::Button("Button")) counter++;
+				ImGui::SameLine();
+				ImGui::Text("counter = %d", counter);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", static_cast<double>(1000.0f / io.Framerate),
+							static_cast<double>(io.Framerate));
+				ImGui::End();
+			}
+
+			{
+				ImGui::Begin("Hello, world1!");			  // Create a window called "Hello, world!" and append into it.
+				ImGui::Text("This is some useful text."); // Display some text (you can use a format strings too)
+
+				ImGui::End();
+			}
+			// Rendering
+			ImGui::Render();
+			int displayW, displayH;
+			glfwGetFramebufferSize(window, &displayW, &displayH);
+			glViewport(0, 0, displayW, displayH);
+			glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+			glfwSwapBuffers(window);
+		}
+		// Cleanup
+		ImGui_ImplOpenGL3_Shutdown();
+		ImGui_ImplGlfw_Shutdown();
+		ImGui::DestroyContext();
+
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
+
 	static int run(int pArgc, char* pArgv[]) {
 #if defined __linux
 		setenv("GDK_DEBUG", "gl-prefer-gl", false);
@@ -52,7 +160,6 @@ public:
 		Globals::init();
 		signal(SIGABRT, signals);
 		signal(SIGSEGV, signals);
-
 
 		appController = std::make_shared<mvp::ApplicationController>();
 		std::shared_ptr<Gtk::Application> application = Gtk::Application::create("org.gtkmm.example");
@@ -115,6 +222,7 @@ public:
 		auto presenter = std::make_shared<mvp::PresenterStartup>(window, model);
 		appController->run(presenter);
 		//addWindow(presenter);
+		std::thread([]() { runImgui(); }).detach();
 
 
 		//application->add_window(*window->getCreateProjectWindow()->window);
