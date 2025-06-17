369c69ebb85f439fa7c7ca63996db3e7af86e9f2
AlexusYT <93870232+AlexusYT@users.noreply.github.com>
2025-06-17 14:51:11 +0500

    Renamed CMake project and MegaEngine_DEVELOPER_MODE option to KwasarEngine_DEVELOPER_MODE

--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -1,11 +1,11 @@
 # ---- Developer mode ----
 
 # Developer mode enables targets and code paths in the CMake scripts that are
-# only relevant for the developer(s) of MegaEngineRing
+# only relevant for the developer(s) of KwasarEngine
 # Targets necessary to build the project must be provided unconditionally, so
 # consumers can trivially build and package the project
 if (PROJECT_IS_TOP_LEVEL)
-    option(MegaEngine_DEVELOPER_MODE "Enable developer mode" OFF)
+    option(KwasarEngine_DEVELOPER_MODE "Enable developer mode" OFF)
 endif ()
 
 # ---- Warning guard ----
